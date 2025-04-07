3c34fcf2e810cd0e824ea843886536cafcddf5c8
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2025-04-07 16:13:36 +0200

    Added DAO for PaperDeliveryDriverCapacitiesDispatched

---
+++ src/main/java/it/pagopa/pn/delayer/middleware/dao/dynamo/entity/PaperDeliveryDriverCapacitiesDispatched.java
@@ -0,0 +1,36 @@
+package it.pagopa.pn.delayer.middleware.dao.dynamo.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+import java.time.Instant;
+
+@DynamoDbBean
+@Data
+public class PaperDeliveryDriverCapacitiesDispatched {
+
+    public static final String COL_DELIVERY_DRIVER_ID_GEOKEY = "deliveryDriverIdGeokey";
+    public static final String COL_DELIVERY_DATE = "deliveryDate";
+    public static final String COL_TENDER_ID = "tenderId";
+    public static final String COL_DELIVERY_DRIVER_ID = "deliveryDriverId";
+    public static final String COL_GEO_KEY = "geoKey";
+    public static final String COL_CAPACITY = "usedCapacity";
+
+
+    @Getter(onMethod = @__({@DynamoDbPartitionKey, @DynamoDbAttribute(COL_DELIVERY_DRIVER_ID_GEOKEY)}))
+    private String deliveryDriverIdGeokey;
+    @Getter(onMethod = @__({@DynamoDbSortKey, @DynamoDbAttribute(COL_DELIVERY_DATE)}))
+    private Instant deliveryDate;
+    @Getter(onMethod = @__({@DynamoDbAttribute(COL_TENDER_ID)}))
+    private String tenderId;
+    @Getter(onMethod = @__({@DynamoDbAttribute(COL_DELIVERY_DRIVER_ID)}))
+    private String deliveryDriverId;
+    @Getter(onMethod = @__({@DynamoDbAttribute(COL_GEO_KEY)}))
+    private String geokey;
+    @Getter(onMethod = @__({@DynamoDbAttribute(COL_CAPACITY)}))
+    private int usedCapacity;
+}
---
+++ src/test/java/it/pagopa/pn/delayer/middleware/dao/PaperDeliveryDriverCapacitiesDispatchedDaoIT.java
@@ -0,0 +1,57 @@
+package it.pagopa.pn.delayer.middleware.dao;
+
+import it.pagopa.pn.delayer.BaseTest;
+import it.pagopa.pn.delayer.middleware.dao.dynamo.PaperDeliveryDriverCapacitiesDispatchedDAO;
+import it.pagopa.pn.delayer.middleware.dao.dynamo.entity.PaperDeliveryDriverCapacitiesDispatched;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.test.StepVerifier;
+
+import java.time.Instant;
+import java.util.List;
+
+
+class PaperDeliveryDriverCapacitiesDispatchedDaoIT extends BaseTest.WithLocalStack {
+
+    @Autowired
+    PaperDeliveryDriverCapacitiesDispatchedDAO paperDeliveryDriverCapacitiesDispatchedDao;
+
+    @Test
+    void testUpdateAndGet() {
+        PaperDeliveryDriverCapacitiesDispatched entity = new PaperDeliveryDriverCapacitiesDispatched();
+        entity.setDeliveryDriverIdGeokey("pk");
+        Instant deliveryDate = Instant.parse("2025-04-07T00:00:00Z");
+        entity.setDeliveryDate(deliveryDate);
+
+        paperDeliveryDriverCapacitiesDispatchedDao.updateCounter("pk", deliveryDate, 5).block();
+
+        PaperDeliveryDriverCapacitiesDispatched response = paperDeliveryDriverCapacitiesDispatchedDao.get("pk", deliveryDate).block();
+        assert response != null;
+        Assertions.assertEquals(entity.getDeliveryDriverIdGeokey(), response.getDeliveryDriverIdGeokey());
+        Assertions.assertEquals(entity.getDeliveryDate(), response.getDeliveryDate());
+        Assertions.assertEquals(5, response.getUsedCapacity());
+
+        paperDeliveryDriverCapacitiesDispatchedDao.updateCounter("pk", deliveryDate, 5).block();
+
+        PaperDeliveryDriverCapacitiesDispatched response2 = paperDeliveryDriverCapacitiesDispatchedDao.get("pk", deliveryDate).block();
+        assert response2 != null;
+        Assertions.assertEquals(entity.getDeliveryDriverIdGeokey(), response2.getDeliveryDriverIdGeokey());
+        Assertions.assertEquals(entity.getDeliveryDate(), response2.getDeliveryDate());
+        Assertions.assertEquals(10, response2.getUsedCapacity());
+    }
+
+    @Test
+    void testBatchGetItem() {
+        List<String> pks = List.of("test-pk1", "test-pk2", "test-pk3");
+        Instant deliveryDate = Instant.now();
+
+        paperDeliveryDriverCapacitiesDispatchedDao.updateCounter("test-pk1", deliveryDate, 5).block();
+        paperDeliveryDriverCapacitiesDispatchedDao.updateCounter("test-pk2", deliveryDate, 5).block();
+
+        StepVerifier.create(paperDeliveryDriverCapacitiesDispatchedDao.batchGetItem(pks, deliveryDate))
+                .expectNextCount(2)
+                .expectComplete()
+                .verify();
+    }
+}
