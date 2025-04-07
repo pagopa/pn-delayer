79cc144100be786765067aafa265ca2a63295cab
DacunzoMa <122782376+DacunzoMa@users.noreply.github.com>
2025-04-07 14:43:05 +0200

    [PN-14473] Created DynamoDb tables and init for localstack (#5)
    * PN-14473 - Created DynamoDb tables and init for localstack
    
    * PN-14473 - fixed package
    
    * prova CI
    
    * PN-14473 add CODEOWNERS and DelayerApplicationTests
    
    ---------
    
    Co-authored-by: flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
    Co-authored-by: Vincenzo Racca <vincenzo.racca@pagopa.it>

---
+++ src/test/resources/testcontainers/init.sh
@@ -0,0 +1,52 @@
+echo " - Create pn-delayer TABLES"
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name pn-PaperDeliveryDriverCapacities  \
+    --attribute-definitions \
+        AttributeName=pk,AttributeType=S \
+        AttributeName=activationDateFrom,AttributeType=S \
+    --key-schema \
+        AttributeName=pk,KeyType=HASH \
+        AttributeName=activationDateFrom,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name pn-PaperDeliveryDriverCapacitiesDispatched  \
+    --attribute-definitions \
+        AttributeName=deliveryDriverIdGeokey,AttributeType=S \
+        AttributeName=deliveryDate,AttributeType=S \
+    --key-schema \
+        AttributeName=deliveryDriverIdGeokey,KeyType=HASH \
+        AttributeName=deliveryDate,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name pn-PaperDeliveryHighPriority  \
+    --attribute-definitions \
+        AttributeName=deliveryDriverIdGeokey,AttributeType=S \
+        AttributeName=createdAt,AttributeType=S \
+    --key-schema \
+        AttributeName=deliveryDriverIdGeokey,KeyType=HASH \
+        AttributeName=createdAt,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
+    dynamodb create-table \
+    --table-name pn-PaperDeliveryReadyToSend  \
+    --attribute-definitions \
+        AttributeName=deliveryDate,AttributeType=S \
+        AttributeName=requestId,AttributeType=S \
+    --key-schema \
+        AttributeName=deliveryDate,KeyType=HASH \
+        AttributeName=requestId,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+
+echo "Initialization terminated"
