df740464c256e43f9827ecda4073138fb631b2e4
DacunzoMa <matteo.dacunzo@emeal.nttdata.com>
2025-04-07 12:23:38 +0200

    PN-14477 - Created PaperDeliveryDriverCapacitiesDispatched table and DAO & Added classes for localstack testing

--- src/main/java/it/pagopa/pn/delayer/middleware/dao/dynamo/PaperDeliveryDriverCapacitiesDispatchedDAO.java
+++ src/main/java/it/pagopa/pn/delayer/middleware/dao/dynamo/PaperDeliveryDriverCapacitiesDispatchedDAO.java
@@ -3,7 +3,6 @@ package it.pagopa.pn.delayer.middleware.dao.dynamo;
 import it.pagopa.pn.delayer.middleware.dao.dynamo.entity.PaperDeliveryDriverCapacitiesDispatched;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.services.dynamodb.model.UpdateItemResponse;
 
 import java.time.Instant;
