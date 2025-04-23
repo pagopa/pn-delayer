4c25d98c8b21a68ea4551514d6267dc08e88e98a
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2025-04-23 12:40:03 +0200

    add PN_DELAYER_UNIFIEDDELIVERYDRIVERPROVINCE env

--- src/main/java/it/pagopa/pn/delayer/PaperDeliveryJobRunner.java
+++ src/main/java/it/pagopa/pn/delayer/PaperDeliveryJobRunner.java
@@ -1,16 +1,21 @@
 package it.pagopa.pn.delayer;
 
+import it.pagopa.pn.commons.utils.MDCUtils;
+import it.pagopa.pn.delayer.config.PnDelayerConfigs;
 import it.pagopa.pn.delayer.service.HighPriorityBatchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import reactor.core.publisher.Mono;
 
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -19,18 +24,41 @@ public class PaperDeliveryJobRunner implements CommandLineRunner {
 
     private final HighPriorityBatchService highPriorityBatchService;
     private final ApplicationContext applicationContext;
+    private final PnDelayerConfigs pnDelayerConfigs;
 
     @Override
     public void run(String... args) {
-        if(args.length != 0 && StringUtils.isNotBlank(args[0])){
-            log.info("Starting batch for pk: {}", args[0]);
-            var startExecutionBatch = Instant.now();
-            highPriorityBatchService.initHighPriorityBatch(args[0], new HashMap<>(), startExecutionBatch).block();
-            int exitCode = SpringApplication.exit(applicationContext, () -> 0);
-            System.exit(exitCode);
+        int exitCode;
+        String unifiedDeliveryDriverProvince = pnDelayerConfigs.getUnifiedDeliveryDriverProvince();
+        addMDC(unifiedDeliveryDriverProvince);
+        if (StringUtils.hasText(unifiedDeliveryDriverProvince)) {
+            exitCode = doExecute(unifiedDeliveryDriverProvince);
         } else {
             log.warn("No pk provided, cannot start batch");
+            exitCode = SpringApplication.exit(applicationContext, () -> 1);
         }
+        System.exit(exitCode);
+    }
+
+    private int doExecute(String unifiedDeliveryDriverProvince) {
+        try {
+            log.info("Starting batch for pk: {}", unifiedDeliveryDriverProvince);
+            var startExecutionBatch = Instant.now();
+            Mono<Void> monoExcecution = highPriorityBatchService.initHighPriorityBatch(unifiedDeliveryDriverProvince, new HashMap<>(), startExecutionBatch);
+            MDCUtils.addMDCToContextAndExecute(monoExcecution).block();
+            return 0;
+        }
+        catch (Exception e) {
+            log.error("Error while executing batch", e);
+            return 1;
+        }
+    }
+
+    private void addMDC(String unifiedDeliveryDriverProvince) {
+        MDCUtils.clearMDCKeys();
+        MDC.put(MDCUtils.MDC_TRACE_ID_KEY, StringUtils.hasText(System.getenv("AWS_BATCH_JOB_ID"))
+                ? System.getenv("AWS_BATCH_JOB_ID") : UUID.randomUUID().toString());
+        MDC.put(MDCUtils.MDC_PN_CTX_REQUEST_ID, unifiedDeliveryDriverProvince);
     }
 }
 
