f559c444611150d43e8c16e75ba8c22c02553044
flaminiaScarciofolo <113031535+flaminiaScarciofolo@users.noreply.github.com>
2025-05-12 14:34:19 +0200

    [PN-14893] Added DelayerToPaperChannelRecovery lambda (#42)
    * [PN-14893] Added DelayerToPaperChannelRecovery lambda
    
    * [PN-14893] Fixed permission in microservice.yml, fixed ids for sqs sendMessageBatch

---
+++ functions/delayerToPaperChannelRecoveryLambda/src/app/lib/sqsSender.js
@@ -0,0 +1,51 @@
+const { SQSClient, SendMessageBatchCommand } = require("@aws-sdk/client-sqs");
+const utils = require("./utils");
+
+const sqsClient = new SQSClient({});
+
+async function prepareAndSendSqsMessages(items) {
+    const results = { successes: [], failures: [] };
+    const messageBatches = utils.chunkArray(prepareSqsMessages(items),10);
+
+    for (const batch of messageBatches) {
+        const batchMap = new Map();
+        let idCounter = 0;
+        const entries = batch.map(entry => {
+            const id = (idCounter++).toString();
+            batchMap.set(id, entry);
+            return { Id: id, MessageBody: JSON.stringify(entry) };
+        });
+        try {
+            const sqsResponse = await sqsClient.send(new SendMessageBatchCommand({
+                QueueUrl: process.env.DELAYERTOPAPERCHANNEL_QUEUEURL,
+                Entries: entries,
+            }));
+
+            sqsResponse.Failed?.forEach(failure => {
+                const item = batchMap.get(failure.Id);
+                if (item) results.failures.push(item.requestId);
+            });
+
+            sqsResponse.Successful?.forEach(success => {
+                const item = batchMap.get(success.Id);
+                if (item) results.successes.push(item.requestId);
+            });
+
+            console.log(`Successfully sent ${sqsResponse.Successful.length} messages`);
+        } catch (error) {
+            console.error("Error during SendMessageBatch:", error);
+            batch.forEach(item => results.failures.push(item.requestId));
+        }
+    }
+    return results;
+}
+
+function prepareSqsMessages(items) {
+  return items.map((item) => ({
+      requestId: item.requestId,
+      iun: item.iun,
+      attemptRetry: 0
+  }));
+}
+
+module.exports = {prepareAndSendSqsMessages};
