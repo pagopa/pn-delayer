cda84a6a521ed2c37f0d01360d465876dd628dfc
Vincenzo Racca <vincenzo.racca@pagopa.it>
2025-11-27 17:55:32 +0100

    PN-17404 add paperDeliveryDriverUsedCapacitiesTableName param in GET_USED_CAPACIY

--- functions/testDelayerLambda/src/app/getUsedCapacity.js
+++ functions/testDelayerLambda/src/app/getUsedCapacity.js
@@ -2,7 +2,6 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, GetCommand } = require("@aws-sdk/lib-dynamodb");
 
-const TABLE_NAME = "pn-PaperDeliveryDriverUsedCapacities";
 const ddbClient = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(ddbClient);
 
@@ -11,13 +10,13 @@ const docClient = DynamoDBDocumentClient.from(ddbClient);
  * @param {Array<string>} params [unifiedDeliveryDriver, geoKey, deliveryDate]
  */
 async function getUsedCapacity(params = []) {
-    const [unifiedDeliveryDriver, geoKey, deliveryDate] = params;
-    if (!unifiedDeliveryDriver || !geoKey || !deliveryDate) {
-        throw new Error("Parameters must be [unifiedDeliveryDriver, geoKey, deliveryDate]");
+    const [paperDeliveryDriverUsedCapacitiesTableName, unifiedDeliveryDriver, geoKey, deliveryDate] = params;
+    if (!paperDeliveryDriverUsedCapacitiesTableName || !unifiedDeliveryDriver || !geoKey || !deliveryDate) {
+        throw new Error("Parameters must be [paperDeliveryDriverUsedCapacitiesTableName, unifiedDeliveryDriver, geoKey, deliveryDate]");
     }
     const partitionKey = `${unifiedDeliveryDriver}~${geoKey}`;
     const command = new GetCommand({
-        TableName: TABLE_NAME,
+        TableName: paperDeliveryDriverUsedCapacitiesTableName,
         Key: {
             unifiedDeliveryDriverGeokey: partitionKey,
             deliveryDate: deliveryDate,
