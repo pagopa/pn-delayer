0bd1c1a0fbbb52442c051b71797ce4e9d860887e
Vincenzo Racca <vincenzo.racca@pagopa.it>
2026-04-28 15:35:36 +0200

    fix aws local props

--- src/main/java/it/pagopa/pn/delayer/config/springbootcfg/AwsConfigsActivation.java
+++ src/main/java/it/pagopa/pn/delayer/config/springbootcfg/AwsConfigsActivation.java
@@ -3,8 +3,10 @@ package it.pagopa.pn.delayer.config.springbootcfg;
 import it.pagopa.pn.commons.configs.aws.AwsConfigs;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @ConfigurationProperties("aws")
+@Profile("!aws")
 public class AwsConfigsActivation extends AwsConfigs {
 }
